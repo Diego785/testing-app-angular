fbd9de8c52c118a17e643dda0872d6ae6f440862
Diego785 <diegohurtado2412@gmail.com>
2022-06-07 01:12:48 -0400

    commiting my testing app from Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testing-app';
+
+  data: any[] = [];
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit(){
+    this.http.get('https://api.spacexdata.com/v4/rockets')
+    .subscribe((data: any) =>{
+      this.data = data;
+    })
+  }
 }
